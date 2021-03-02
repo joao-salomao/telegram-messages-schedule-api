5d9f3e18c21d97af4c518479c97cee317cee5f47
helenatamanini <75807270+helenatamanini@users.noreply.github.com>
2021-03-01 21:18:23 -0300

    finalizando crud de mensagens

--- src/app.js
+++ src/app.js
@@ -38,3 +38,77 @@ app.get('/messages', async (req, res) => {
         return res.status(500).send(error)
     }
 })
+// Rota que pega uma única mensagem cadastrada BD
+app.get('/messages/:id', async (req, res) => {
+    try {
+        const message = await new Message({ id: req.params.id }).fetch({ withRelated: 'groups' })
+        return res.send(message)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+// Rota que cadastra uma mensagem no BD
+app.post('/messages', async (req, res) => {
+    try {
+        const data = {
+            title: req.body.title,
+            content: req.body.content,
+            date_time: req.body.date_time
+        }
+
+        const message = await new Message(data).save()
+
+        if (req.body.groups) {
+            await message.groups().attach(req.body.groups)
+            await message.load('groups')
+        }
+
+        return res.send(message)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error)
+    }
+})
+
+// Rota que atualiza uma mensagem no BD
+app.put('/messages/:id', async (req, res) => {
+    try {
+        const message = await new Message({ id: req.params.id })
+            .save({
+                title: req.body.title,
+                content: req.body.content,
+                date_time: req.body.date_time
+            })
+
+        if (req.body.groups) {
+            await message.groups().detach()
+            await message.groups().attach(req.body.groups)
+            await message.load('groups')
+        }
+
+        return res.send(message)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+// Rota que deleta uma mensagem no BD
+app.delete('/messages/:id', async (req, res) => {
+    try {
+        new Message({
+            id: req.params.id
+        }).destroy()
+
+        res.send("Mensagem deletada")
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
+})
+
+// Começa a ouvir as requisições que chegam na porta especificada
+const port = process.env.PORT || 9000
+app.listen(port, () => {
+    console.log("Listening on port: " + port)
+})
